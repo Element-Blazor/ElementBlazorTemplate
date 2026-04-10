b73f5bf2b37a54c2864ebadb1ef70a2b719f3552
maikebing <mysticboy@live.com>
2026-04-10 15:52:35 +0800

    Refactor code structure for improved readability and maintainability

---
+++ Admin/Admin/RoleModel.cs
@@ -0,0 +1,17 @@
+﻿using Element;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Element.Admin
+{
+    public class RoleModel
+    {
+        public string Id { get; set; }
+
+        [TableColumn(Text = "名称")]
+        public string Name { get; set; }
+
+        public List<string> Resources { get; set; } = new List<string>();
+    }
+}
---
+++ Admin/Admin/RoleResource.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Element.Admin
+{
+    public class RoleResource
+    {
+        public string RoleId { get; set; }
+        public string ResourceId { get; set; }
+    }
+}
